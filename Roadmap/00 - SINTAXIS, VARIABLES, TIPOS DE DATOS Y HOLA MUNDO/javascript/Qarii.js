a907bac2bedf4172d0c6c4c22d0ab99b980c6154
Kary <qqaryrodriguez@gmail.com>
2024-05-20 16:29:31 -0600

    Create Qarii.js
    Primer reto <3

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Qarii.js
@@ -0,0 +1,34 @@
+// URL de la página oficial del lenguaje https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...). 
+
+/* 
+Comentario 
+de varias líneas
+y hasta un emoji 🐱‍👤😊
+*/
+
+
+// Variables y constantes 
+/*
+let → declarar variable local 
+var → declarar variable global
+const →  declarar variables locales y globales dependiendo del contexto de ejecución
+*/
+
+// - Crea una variable (y una constante si el lenguaje lo soporta).
+
+var unaVariable = "valor de la variable";
+const unaConstante = "valor de tu constante";
+
+// - Crea variables representando todos los tipos de datos primitivos (cadenas de texto, enteros, booleanos...)
+
+let cadenaDeTexto = "Your Mom";
+let numeroEntero = 88;
+let operadorBooleano = true;
+let nulo = null;
+let vacio = undefined;
+let simbolo = Symbol();
+
+// - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("Hello, JS :D!");
